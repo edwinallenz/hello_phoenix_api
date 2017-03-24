5bc28cabad23cbfd674704d208c1dc22681cd01f
Fred Allen <edwin.allen@gmail.com>
2017-03-24 13:47:29 -0600

    Hellow Worl API scaffold

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule HelloPhoenixApi.PageControllerTest do
+  use HelloPhoenixApi.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
