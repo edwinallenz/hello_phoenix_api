cc44c376affaee1ba699693ecaa18c5591a97f1c
Fred Allen <edwin.allen@gmail.com>
2017-03-24 17:59:00 -0600

    Remove html stuff Use root route for the API endpoint 	modified:   web/router.ex

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,14 @@
 defmodule HelloPhoenixApi.Router do
   use HelloPhoenixApi.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", HelloPhoenixApi do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", HelloController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", HelloPhoenixApi do
-  #   pipe_through :api
-  # end
 end
